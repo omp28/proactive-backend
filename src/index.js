04d1bb247aa2f25bb4b1fa7e7021fa6ddfcc6013
Om Patel <om28patel2004@gmail.com>
2024-12-06 23:33:57 +0530

    Initial setup:docker and database connection

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+const sequelize = require("./config/db");
+
+const PORT = process.env.PORT || 3000;
+
+sequelize.sync({ alter: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
